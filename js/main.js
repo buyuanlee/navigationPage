3187f1fc014fd8d22b40f4da7b2b10c332f99da0
buyuanlee <buyuanlee@gmail.com>
2018-08-12 00:59:43 +0800

    优化语法，使用js

--- js/main.js
+++ js/main.js
@@ -1,53 +1,129 @@
-var keys = [
-  ['q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', ],
-  ['a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ],
-  ['z', 'x', 'c', 'v', 'b', 'n', 'm', ],
-]
+/**************初始化数据**********/
+
+var hashX = init()
+var keys = hashX.keys
+var hash = hashX.hash
+
+/**********生成键盘**********/
+
+generateKbd(keys, hash)
+
+/*************监听用户*************/
+
+listenToUser(hash)
 
 
-var hash = {
-  a: 'www.acfun.cn',
-  b: 'www.bilibili.com',
-  c: 'www.cilimao.me',
-  d: 'www.douyu.com',
-  f: 'facebook.com',
-  g: 'google.com',
-  y: 'youtube.com',
-}
-//取出hashInLocalStorage中newHash对用的hash
-var hashInLocalStorage = JSON.parse(localStorage.getItem('newHash') || 'null')
-if (hashInLocalStorage) {
-  hash = hashInLocalStorage
-}
-var i = 0;
-while (i < keys.length) {
-  divx = document.createElement('div');
-  content.appendChild(divx);
-  row = keys[i];
-  j = 0;
-  while (j < row.length) {
-    kbdx = document.createElement('kbd');
-    kbdx.textContent = row[j];
-    btnx = document.createElement('button');
-    btnx.textContent = '编辑';
-    btnx.id = row[j];
-    btnx.onclick = function(btnclick) {
-      key = btnclick['target']['id']; //btnclick.target=this
-      x = prompt('give me a website');
-      hash[key] = x;
-      localStorage.setItem('newHash', JSON.stringify(hash))
-      console.log(hash)
-    }
-    kbdx.appendChild(btnx);
-    divx.appendChild(kbdx);
-    j = j + 1;
+
+
+
+/*************工具函数*************/
+
+function init(){
+  var keys = [
+    ['q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', ],
+    ['a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ],
+    ['z', 'x', 'c', 'v', 'b', 'n', 'm', ],
+  ]
+  var hash = {
+    a: 'www.acfun.cn',b: 'www.bilibili.com',
+    d: 'www.douyu.com',  f: 'www.facebook.com',
+    g: 'www.google.com',y: 'www.youtube.com',
+    z: 'www.zhihu.com',p: 'www.panda.tv',
+    q: 'www.qq.com',l: 'lol.qq.com',
+    x: 'xiaomi.com',s: 'store.steampowered.com',
+    o: 'opera.com',h: 'huya.com',
+    w: 'weibo.com',  e: 'ele.me',
+    r: 'yyets.com',n: 'www.nass.usda.gov',
+    t: 'taobao.com',u: 'www.ui.cn',
+    i: 'www.icloud.com',  j: 'jquery.com',
+    k: 'kuaishou.com',c: 'www.china.com',
+    v: 'www.vmovier.com',m: 'www.imooc.com',
   }
-  i = i + 1;
+
+//取出localStorage中newHash对应的hash
+  var hashInLocalStorage = getFormLocalStorage('newHash');
+  if (hashInLocalStorage) {
+    hash = hashInLocalStorage
+  }
+return{
+  "keys":keys,
+  "hash":hash,
 }
-document.onkeypress = function(presskey) {
-  key = presskey.key;
-  website = hash[key];
-  console.log(website);
-  //location.href=website;当前窗口打开
-  window.open('http://' + website, '_blank')
+}
+
+/********存取用户输入数据函数*********/
+function getFormLocalStorage(name) {
+  return JSON.parse(localStorage.getItem(name) || 'null')
+}
+
+/********创建变量缩写函数*********/
+function tag(tagName) {
+  return document.createElement(tagName)
+}
+
+/********创建button函数*********/
+function createBtn(id) {
+  var btnx = tag('button');
+  btnx.textContent = '编辑';
+  btnx.id = id;
+  btnx.onclick = function() {
+    var img1 = this.previousSibling;
+    var key = this.id;
+    var x = prompt('请输入要保存的网址');
+    hash[key] = x;
+    localStorage.setItem('newHash', JSON.stringify(hash))
+    console.log(hash)
+  }
+  return btnx
+}
+
+/********创建image函数*********/
+function createImg(domain) {
+  var imgx = tag('img');
+  if (domain) {
+    imgx.src = 'http://' + domain + '/favicon.ico';
+  } else {
+    imgx.src = 'http://91jean.oss-cn-hangzhou.aliyuncs.com/18-8-11/46197852.jpg';
+  }
+  imgx.onerror = function() {
+    this.src = 'http://91jean.oss-cn-hangzhou.aliyuncs.com/18-8-11/46197852.jpg';
+  }
+  return imgx
+}
+
+
+/********创建新hash函数*********/
+function generateKbd(keys,hash){
+  for (var i = 0; i < keys.length; i++) {
+    var divx = tag('div');
+
+    content.appendChild(divx);
+
+    var row = keys[i];
+    for (var j = 0; j < row.length; j++) {
+
+      var btnx = createBtn(row[j])
+
+      var imgx = createImg(hash[row[j]])
+
+      var kbdx = tag('kbd');
+      kbdx.textContent = row[j];
+      kbdx.appendChild(imgx);
+      kbdx.appendChild(btnx);
+
+      divx.appendChild(kbdx);
+    }
+  }
+}
+
+
+/********监听用户函数*********/
+function listenToUser(hash){
+  document.onkeypress = function(presskey) {
+    var key = presskey.key;
+    var website = hash[key];
+    console.log(website);
+    //location.href=website;当前窗口打开
+    window.open('http://' + website, '_blank')
+  }
 }
